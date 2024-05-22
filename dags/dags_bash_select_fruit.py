e4d66d10fe9579797fe6092926605cf1478f4942
vanko1721 <vanko1721@penta.co.kr>
2024-05-22 13:57:11 +0900

    select_fruit.sh upload

---
+++ dags/dags_bash_select_fruit.py
@@ -0,0 +1,24 @@
+#from airflow.models.dag import DAG
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_select_fruit",
+    schedule="10 0 * * 6#1",
+    start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    t1_orange = BashOperator(
+        task_id = "t1_orange",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORAGE",
+    )
+
+    t1_avocado = BashOperator(
+        task_id = "t1_avocado",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
+    )
+
+    t1_orange >> t1_avocado
