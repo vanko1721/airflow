74043af84a164d6c33c0187fc33b2e1212109d3d
vanko1721 <vanko1721@penta.co.kr>
2024-05-23 13:46:34 +0900

    05_23 YJS upload

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -7,7 +7,7 @@ with DAG(
     dag_id="dags_bash_operator",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
-    catchup=False,
+    catchup=False
 ) as dag:
     
     # [START howto_operator_bash]
