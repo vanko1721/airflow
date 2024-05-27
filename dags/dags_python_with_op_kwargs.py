a3197b5f6c613e3d646b68febebd2e59c5658d7c
vanko1721 <vanko1721@penta.co.kr>
2024-05-27 10:28:09 +0900

    op_kwargs test 2 upload

--- dags/dags_python_with_op_kwargs.py
+++ dags/dags_python_with_op_kwargs.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 from common.common_func import regist2
 
 with DAG(
-    dag_id="dags_python_with_op_args",
+    dag_id="dags_python_with_op_kwargs",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
     catchup=False
