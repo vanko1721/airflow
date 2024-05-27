c57aa37db81109c83c03d10f671702172a2dbfd6
vanko1721 <vanko1721@penta.co.kr>
2024-05-27 10:06:58 +0900

    op_args test 2 upload

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -13,7 +13,7 @@ with DAG(
     
     regist_t1 = PythonOperator(
         task_id = 'regist_t1',
-        python_callable=regis,
+        python_callable=regist,
         op_args=['YJS','MAN','kr','seoul']
     )
 
