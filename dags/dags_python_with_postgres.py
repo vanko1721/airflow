be7e30440925da6e3aef622caeb33f2ac2ca2f85
vanko1721 <vanko1721@penta.co.kr>
2025-01-02 09:58:44 +0900

    20250102 01

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -19,7 +19,7 @@ with DAG(
                 dag_id = kwargs.get('ti').dag_id
                 task_id = kwargs.get('ti').task_id
                 run_id = kwargs.get('ti').run_id
-                msg = 'insrt 수행'
+                msg = 'insert 수행'
                 sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
                 cursor.execute(sql,(dag_id,task_id,run_id,msg))
                 conn.commit()
