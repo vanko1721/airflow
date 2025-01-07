d0d2dfbc1cdd13936eed0c067614c7e3f58c998b
vanko1721 <vanko1721@penta.co.kr>
2025-01-07 10:06:16 +0900

    20250107 11

--- dags/dags_test_value_select.py
+++ dags/dags_test_value_select.py
@@ -20,7 +20,7 @@ with DAG(
                 task_id = kwargs.get('ti').task_id
                 run_id = kwargs.get('ti').run_id
                 val1 = 'col2'
-                val2 = 1
+                val2 = '1'
                 #msg = 'insert 수행'
                 #sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
                 #cursor.execute(sql,(dag_id,task_id,run_id,msg))
