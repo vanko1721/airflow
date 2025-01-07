091bdd088e373e23b2ea4f56fca847795d925ce3
vanko1721 <vanko1721@penta.co.kr>
2025-01-07 09:51:08 +0900

    20250107 06

--- dags/dags_test_value_select.py
+++ dags/dags_test_value_select.py
@@ -19,15 +19,15 @@ with DAG(
                 dag_id = kwargs.get('ti').dag_id
                 task_id = kwargs.get('ti').task_id
                 run_id = kwargs.get('ti').run_id
-                #val1 = col2
-                #val2 = 1
+                val1 = 'col2'
+                val2 = 1
                 #msg = 'insert 수행'
                 #sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
                 #cursor.execute(sql,(dag_id,task_id,run_id,msg))
-                #sql = 'select %s from value_test where col1 = %s'
-                sql = 'select col2 from value_test where col1 = 1'
-                #cursor.execute(sql,(val1,val2))
-                cursor.execute(sql)
+                sql = 'select %s from value_test where col1 = %s'
+                #sql = 'select col2 from value_test where col1 = 1'
+                cursor.execute(sql,(val1,val2))
+                #cursor.execute(sql)
                 result = cursor.fetchall()
                 #sql1 = 'select %s'
                 #cursor.execute(sql1,(result))
