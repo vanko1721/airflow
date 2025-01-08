dd49055d3fb4796a6d8271c668e724a7add1757e
vanko1721 <vanko1721@penta.co.kr>
2025-01-08 15:38:33 +0900

    1

--- dags/dags_test_value_select.py
+++ dags/dags_test_value_select.py
@@ -27,8 +27,8 @@ with DAG(
                 cursor.execute(sql)
                 rows = cursor.fetchall()
                 result = rows[0][0] if rows else None
-                sql1 = 'select %s FROM value_test2'
-                cursor.execute(sql1,str(result))
+                sql1 = 'select ' + result + ' FROM value_test2'
+                cursor.execute(sql1)
                 rows1 = cursor.fetchall()
                 print(rows1)
                 #result1 = rows1[0][0] if rows1 else None
